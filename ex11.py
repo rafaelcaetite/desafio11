09b1785cb59b0d468218cae3e9eb083f93a55845
Rafael Martins <102500171+rafaelcaetite@users.noreply.github.com>
2022-03-28 20:51:50 -0300

    Add files via upload

---
+++ ex11.py
@@ -0,0 +1,4 @@
+n1=float(input('Quantos metros de largura tem sua parede?'))
+n2=float(input('Quantos metros de altura tem sua parede?'))
+n3=n1*n2
+print(f'A área da sua parede é de {n3}m² e você precisa de {n3/2} litros de tinta para pintá-la.')
